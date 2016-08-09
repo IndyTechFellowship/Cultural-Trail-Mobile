85b04395b4f553671513ae697dd538fe34d36654
Tylor Garrett <tylorgarrett@gmail.com>
2016-08-08 20:35:34 -0400

    added login page and image

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,16 +2,48 @@ import React, { Component } from 'react';
 
 import {
 	StyleSheet,
+	Image,
  	Text,
+ 	TextInput,
   View
 } from 'react-native';
 
 export default class LoginScene extends Component {
+	
 	render(){
 		return (
-			<View>
-				<Text>Hello, World</Text>
+			<View style={styles.container}>
+				<Image
+					style={styles.headerImage}
+          source={require('../images/ict-logo.png')}
+        />
+				<TextInput
+        	style={styles.textInput}
+        	placeholder="Email" />
+        <TextInput
+        	style={styles.textInput}
+        	placeholder="Password" />
 			</View>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff'
+	},
+	textInput: {
+		height: 48,
+		borderColor: 'gray',
+		borderWidth: 1,
+		paddingLeft: 75,
+		paddingRight: 75,
+		flex: 0
+	},
+	headerImage: {
+		width: 400,
+    height: 400,
+    alignItems: 'center'
+	}
+})
