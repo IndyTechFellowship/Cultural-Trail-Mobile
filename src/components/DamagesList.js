4cfa56a4c25050d92f2cc9b18130b933a38f5383
Ashley Zegiestowsky <ashleyzeg@gmail.com>
2016-10-24 19:50:26 -0400

    add damages list

---
+++ src/components/DamagesList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import {
+    StyleSheet,
+    View,
+    Text
+} from 'react-native';
+
+export default class DamagesList extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text>Hey, it's the damages</Text>
+            </View>
+        )
+    }
+}
